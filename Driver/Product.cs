5b7acf04ba3fecce9002e8c0be2de4d36e8e78c8
Wei Liao <wliao008@gmail.com>
2013-12-18 17:01:53 -0500

    more on the driver prog

--- Driver/Product.cs
+++ Driver/Product.cs
@@ -11,21 +11,36 @@ namespace Driver
 {
     using System;
     using System.Collections.Generic;
+using ProtoBuf;
     
+    [ProtoContract]
     public partial class Product
     {
+        [ProtoMember(1)]
         public int ID { get; set; }
+        [ProtoMember(2)]
         public int VendorID { get; set; }
+        [ProtoMember(3)]
         public int ProductTypeID { get; set; }
+        [ProtoMember(4)]
         public Nullable<int> ProductSubTypeID { get; set; }
+        [ProtoMember(5)]
         public string Code { get; set; }
+        [ProtoMember(6)]
         public string InternalName { get; set; }
+        [ProtoMember(7)]
         public string ExternalShortName { get; set; }
+        [ProtoMember(8)]
         public string ExternalFullName { get; set; }
+        [ProtoMember(9)]
         public bool Locked { get; set; }
+        [ProtoMember(10)]
         public bool Deleted { get; set; }
+        [ProtoMember(11)]
         public string InternalDescription { get; set; }
+        [ProtoMember(12)]
         public string ExternalDescription { get; set; }
+        [ProtoMember(13)]
         public string ExternalDescriptionWmd { get; set; }
     }
 }
--- Driver/Program.cs
+++ Driver/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using Batbeetle;
+using ProtoBuf;
 
 namespace Driver
 {
@@ -11,19 +14,35 @@ namespace Driver
     {
         static void Main(string[] args)
         {
+            DbEntities db = new DbEntities();
+            var consumers = db.Consumers.ToList();
+            var products = db.Products.ToList();
             var before = DateTime.Now;
-            var count = 50000;
-            using (var r = new RedisClient("192.168.1.115"))
+            var count = consumers.Count + products.Count;
+            //using (var r = new RedisClient("192.168.1.115"))
+            //{
+            //    //for (int i = 0; i < count; ++i)
+            //    //{
+            //    //    //Console.WriteLine("Item " + i);
+            //    //    //r.Set("key" + i.ToString(), Guid.NewGuid());
+            //    //}
+            //    r.Set("name", "weiliao3");
+            //    r.Set("num", 10);
+            //    Console.WriteLine("setting name");
+            //}
+
+            using (var ms = new MemoryStream())
             {
-                //for (int i = 0; i < count; ++i)
-                //{
-                //    //Console.WriteLine("Item " + i);
-                //    //r.Set("key" + i.ToString(), Guid.NewGuid());
-                //}
-                r.Set("name", "weiliao3");
-                r.Set("num", 10);
-                Console.WriteLine("setting name");
-            }
+                var formatter = new BinaryFormatter();
+                foreach (var c in consumers)
+                {
+                    //formatter.Serialize(ms, c);
+                    Serializer.Serialize(ms, c);
+                }
+
+                foreach (var p in products)
+                    Serializer.Serialize(ms, p);
+            } 
             var now = DateTime.Now - before;
             Console.WriteLine("Redis (write): took {0} sec {1} ms to store {2} records, total {3} ms",
                 now.Seconds, now.Milliseconds, count, now.TotalMilliseconds);
