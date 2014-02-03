32f359ec3cb652093c24d632c188e3e7a75e77b5
Wei Liao <wliao008@gmail.com>
2014-02-03 11:26:33 -0500

    started on implementing cluster support

---
+++ Batbeetle/Cluster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batbeetle
+{
+    public class Cluster
+    {
+        private readonly ushort RedisClusterHashSlot = 16384;
+        private readonly ushort RedisClusterRequestTtl = 16;
+        private readonly ushort RedisClusterDefaultTimeout = 1;
+
+        public ushort KeySlot(byte[] key)
+        {
+            return (ushort)(Crc16.GetCrc16(key) % RedisClusterHashSlot);
+        }
+    }
+}
