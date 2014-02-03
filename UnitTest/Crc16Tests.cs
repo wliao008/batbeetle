7295ea24c2f6a08bc00262409904fdcb45a197ce
Wei Liao <wliao008@gmail.com>
2014-02-03 11:18:33 -0500

    Added crc16 implementation
    This is migrated from the crc16.rb used in the ruby cluster client.

---
+++ UnitTest/Crc16Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Batbeetle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Crc16Tests
+    {
+        [TestMethod]
+        public void GetCrc16_ShouldReturnUshort()
+        {
+            var result = Crc16.GetCrc16("foo1".ToByte());
+            Assert.AreEqual(62583, result);
+        }
+    }
+}
