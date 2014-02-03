75e288ccb9d18558b332eb48364775f21d773374
Wei Liao <wliao008@gmail.com>
2014-02-03 16:07:22 -0500

    tests

---
+++ UnitTest/ClusterTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Batbeetle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ClusterTests
+    {
+        [TestMethod]
+        public void GetCrc16_ShouldReturnUshort()
+        {
+            var result = Crc16.GetCrc16("foo1".ToByte());
+            Assert.AreEqual(62583, result);
+        }
+
+        [TestMethod]
+        public void KeySlot_GivenAKey_ReturnReturnSlot()
+        {
+            var cluster = new Cluster();
+            var result = cluster.KeySlot("foo".ToByte());
+            Assert.AreEqual(12182, result);
+            var result2 = cluster.KeySlot("hello".ToByte());
+            Assert.AreEqual(866, result2);
+        }
+    }
+}
